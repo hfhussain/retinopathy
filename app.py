558a6ea04b19cd3319e07ead6c2f3f312729c8cc
your-github-hfhussain <your-hoorfatima.hussain@gmail.com>
2025-04-11 16:02:48 +0530

    Fix: bind to 0.0.0.0 and use dynamic port

--- app.py
+++ app.py
@@ -59,3 +59,9 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
+
